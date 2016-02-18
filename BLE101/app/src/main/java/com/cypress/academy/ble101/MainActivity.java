7d507a2688554c0889054ded58eca0b24cff2c68
Greg Landry <gjl@cypress.com>
2016-02-18 15:19:01 -0500

    Add "final" BLE101 project.

--- BLE101/app/src/main/java/com/cypress/academy/ble101/MainActivity.java
+++ BLE101/app/src/main/java/com/cypress/academy/ble101/MainActivity.java
@@ -26,64 +26,78 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import java.util.UUID;
-
 public class MainActivity extends AppCompatActivity {
 
     // TAG is used for informational messages
     private final static String TAG = MainActivity.class.getSimpleName();
 
     // Variables to access objects from the layout such as buttons, switches, values
-    private TextView mCapsenseValue;
-    private Button start_button;
-    private Button search_button;
-    private Button connect_button;
-    private Button discover_button;
-    private Button disconnect_button;
-    private Switch led_switch;
-    private Switch cap_switch;
+    private static TextView mCapsenseValue;
+    private static Button start_button;
+    private static Button search_button;
+    private static Button connect_button;
+    private static Button discover_button;
+    private static Button disconnect_button;
+    private static Switch led_switch;
+    private static Switch cap_switch;
 
     // Variables to manage BLE connection
-    private boolean mConnectState;
-    private boolean mServiceConnected;
-    private BluetoothAdapter mBluetoothAdapter;
-    private BluetoothLeService mBluetoothLeService;
+    private static boolean mConnectState;
+    private static boolean mServiceConnected;
+    private static PSoCCapSenseLedService mPSoCCapSenseLedService;
 
-    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int REQUEST_ENABLE_BLE = 1;
 
     //This is required for Android 6.0 (Marshmallow)
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
 
-    /************************************************************************/
-    /*  Code to manage the BLE Service */
-    /************************************************************************/
+    /**
+     * This manages the lifecycle of the BLE service.
+     * When the service starts we get the service object and initialize the service.
+     */
     private final ServiceConnection mServiceConnection = new ServiceConnection() {
 
+        /**
+         * This is called when the PSoCCapSenseLedService is connected
+         *
+         * @param componentName the component name of the service that has been connected
+         * @param service service being bound
+         */
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder service) {
-            Log.d(TAG, "onServiceConnected");
-            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            Log.i(TAG, "onServiceConnected");
+            mPSoCCapSenseLedService = ((PSoCCapSenseLedService.LocalBinder) service).getService();
             mServiceConnected = true;
-            mBluetoothLeService.initialize();
+            mPSoCCapSenseLedService.initialize();
         }
 
+        /**
+         * This is called when the PSoCCapSenseService is disconnected.
+         *
+         * @param componentName the component name of the service that has been connected
+         */
         @Override
         public void onServiceDisconnected(ComponentName componentName) {
-            Log.d(TAG, "onServiceDisconnected");
-            mBluetoothLeService = null;
+            Log.i(TAG, "onServiceDisconnected");
+            mPSoCCapSenseLedService = null;
         }
     };
 
-    @TargetApi(23) // This is required for Android 6.0 (Marshmallow) to work
+    /**
+     * This is called when the main activity is first created
+     *
+     * @param savedInstanceState is any state saved from prior creations of this activity
+     */
+    @TargetApi(Build.VERSION_CODES.M) // This is required for Android 6.0 (Marshmallow) to work
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* Set up a variable to point to the CapSense value on the display */
+        // Set up a variable to point to the CapSense value on the display
         mCapsenseValue = (TextView) findViewById(R.id.capsense_value);
 
-        /* Set up variables for accessing buttons and slide switches */
+        // Set up variables for accessing buttons and slide switches
         start_button = (Button) findViewById(R.id.start_button);
         search_button = (Button) findViewById(R.id.search_button);
         connect_button = (Button) findViewById(R.id.connect_button);
@@ -92,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
         led_switch = (Switch) findViewById(R.id.led_switch);
         cap_switch = (Switch) findViewById(R.id.capsense_switch);
 
-        /* Initialize service and connection state variable */
+        // Initialize service and connection state variable
         mServiceConnected = false;
         mConnectState = false;
 
@@ -116,26 +130,16 @@ public class MainActivity extends AppCompatActivity {
          /* This will be called when the LED On/Off switch is touched */
         led_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                byte[] value = new byte[1];
-                if (buttonView.isChecked()) {
-                    value[0] = (byte) (1);
-                    Log.i(TAG, "LED On");
-                } else {
-                    value[0] = (byte) (0);
-                    Log.i(TAG, "LED Off");
-                }
-                mBluetoothLeService.writeLedCharacteristic(value);
+                // Turn the LED on or OFF based on the state of the switch
+                mPSoCCapSenseLedService.writeLedCharacteristic(isChecked);
             }
         });
 
          /* This will be called when the CapSense On/Off switch is touched */
         cap_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                boolean value;
-                value = buttonView.isChecked();
-                /* Set notifications in the CCCD locally and write to the server to tell it to notify us */
-                Log.i(TAG, "CapSense Notification " + value);
-                mBluetoothLeService.writeCapSenseNotification(value);
+                 // Turn CapSense Notifications on/off based on the state of the switch
+                mPSoCCapSenseLedService.writeCapSenseNotification(isChecked);
             }
         });
     }
@@ -166,20 +170,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        /* Register the broadcast receiver. This receives messages from the BLE service */
+        // Register the broadcast receiver. This specified the messages the main activity looks for from the PSoCCapSenseLedService
         final IntentFilter filter = new IntentFilter();
-        filter.addAction(BluetoothLeService.ACTION_BLESCAN_CALLBACK);
-        filter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
-        filter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED);
-        filter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED);
-        filter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
+        filter.addAction(PSoCCapSenseLedService.ACTION_BLESCAN_CALLBACK);
+        filter.addAction(PSoCCapSenseLedService.ACTION_CONNECTED);
+        filter.addAction(PSoCCapSenseLedService.ACTION_DISCONNECTED);
+        filter.addAction(PSoCCapSenseLedService.ACTION_SERVICES_DISCOVERED);
+        filter.addAction(PSoCCapSenseLedService.ACTION_DATA_RECEIVED);
         registerReceiver(mBleUpdateReceiver, filter);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // User chose not to enable Bluetooth.
-        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+        if (requestCode == REQUEST_ENABLE_BLE && resultCode == Activity.RESULT_CANCELED) {
             finish();
             return;
         }
@@ -195,90 +199,113 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mBluetoothLeService.close();
+        // Close and unbind the service when the activity goes away
+        mPSoCCapSenseLedService.close();
         unbindService(mServiceConnection);
-        mBluetoothLeService = null;
+        mPSoCCapSenseLedService = null;
+        mServiceConnected = false;
     }
 
-    /************************************************************************/
-    /* Button handler functions and state machine */
-    /************************************************************************/
+    /**
+     * This method handles the start bluetooth button
+     *
+     * @param view the view object
+     */
     public void startBluetooth(View view) {
 
         // Find BLE service and adapter
         final BluetoothManager bluetoothManager =
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        mBluetoothAdapter = bluetoothManager.getAdapter();
+        BluetoothAdapter mBluetoothAdapter = bluetoothManager.getAdapter();
 
         // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
         // fire an intent to display a dialog asking the user to grant permission to enable it.
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLE);
         }
 
         // Start the BLE Service
         Log.d(TAG, "Starting BLE Service");
-        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
+        Intent gattServiceIntent = new Intent(this, PSoCCapSenseLedService.class);
         bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
 
-        /* Disable the start button and turn on the search  button */
+        // Disable the start button and turn on the search  button
         start_button.setEnabled(false);
         search_button.setEnabled(true);
         Log.d(TAG, "Bluetooth is Enabled");
     }
 
+    /**
+     * This method handles the Search for Device button
+     *
+     * @param view the view object
+     */
     public void searchBluetooth(View view) {
         if(mServiceConnected) {
-            mBluetoothLeService.scan();
+            mPSoCCapSenseLedService.scan();
         }
 
         /* After this we wait for the scan callback to detect that a device has been found */
         /* The callback broadcasts a message which is picked up by the mGattUpdateReceiver */
     }
 
+    /**
+     * This method handles the Connect to Device button
+     *
+     * @param view the view object
+     */
     public void connectBluetooth(View view) {
-        mBluetoothLeService.connect();
+        mPSoCCapSenseLedService.connect();
 
         /* After this we wait for the gatt callback to report the device is connected */
         /* That event broadcasts a message which is picked up by the mGattUpdateReceiver */
     }
 
+    /**
+     * This method handles the Discover Services and Characteristics button
+     *
+     * @param view the view object
+     */
     public void discoverServices(View view) {
         /* This will discover both services and characteristics */
-        mBluetoothLeService.discoverServices();
+        mPSoCCapSenseLedService.discoverServices();
 
         /* After this we wait for the gatt callback to report the services and characteristics */
         /* That event broadcasts a message which is picked up by the mGattUpdateReceiver */
     }
 
+    /**
+     * This method handles the Disconnect button
+     *
+     * @param view the view object
+     */
      public void Disconnect(View view) {
-        mBluetoothLeService.disconnect();
+        mPSoCCapSenseLedService.disconnect();
 
         /* After this we wait for the gatt callback to report the device is disconnected */
         /* That event broadcasts a message which is picked up by the mGattUpdateReceiver */
     }
 
-
-    /************************************************************************/
-    /* Listener for BLE event broadcasts */
-    /************************************************************************/
+    /**
+     * Listener for BLE event broadcasts
+     */
     private final BroadcastReceiver mBleUpdateReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
             switch (action) {
-                case BluetoothLeService.ACTION_BLESCAN_CALLBACK:
-                    /* Disable the search button and enable the connect button */
+                case PSoCCapSenseLedService.ACTION_BLESCAN_CALLBACK:
+                    // Disable the search button and enable the connect button
                     search_button.setEnabled(false);
                     connect_button.setEnabled(true);
                     break;
 
-                case BluetoothLeService.ACTION_GATT_CONNECTED:
+                case PSoCCapSenseLedService.ACTION_CONNECTED:
                     /* This if statement is needed because we sometimes get a GATT_CONNECTED */
                     /* action when sending Capsense notifications */
                     if (!mConnectState) {
-                        /* Disable the connect button, enable the discover services and disconnect buttons */
+                        // Dsable the connect button, enable the discover services and disconnect buttons
                         connect_button.setEnabled(false);
                         discover_button.setEnabled(true);
                         disconnect_button.setEnabled(true);
@@ -286,12 +313,12 @@ public class MainActivity extends AppCompatActivity {
                         Log.d(TAG, "Connected to Device");
                     }
                     break;
-                case BluetoothLeService.ACTION_GATT_DISCONNECTED:
-                    /* Disable the disconnect, discover svc, discover char button, and enable the search button */
+                case PSoCCapSenseLedService.ACTION_DISCONNECTED:
+                    // Disable the disconnect, discover svc, discover char button, and enable the search button
                     disconnect_button.setEnabled(false);
                     discover_button.setEnabled(false);
                     search_button.setEnabled(true);
-                    /* Turn off and disable the LED and CapSense switches */
+                    // Turn off and disable the LED and CapSense switches
                     led_switch.setChecked(false);
                     led_switch.setEnabled(false);
                     cap_switch.setChecked(false);
@@ -299,36 +326,28 @@ public class MainActivity extends AppCompatActivity {
                     mConnectState = false;
                     Log.d(TAG, "Disconnected");
                     break;
-                case BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED:
-                    /* Disable the discover services button */
+                case PSoCCapSenseLedService.ACTION_SERVICES_DISCOVERED:
+                    // Disable the discover services button
                     discover_button.setEnabled(false);
-                    /* Enable the LED and CapSense switches */
+                    // Enable the LED and CapSense switches
                     led_switch.setEnabled(true);
                     cap_switch.setEnabled(true);
-                    /* Initiate a read of the current state of the LED from the peer */
-                    /* The onCharacteristicRead callback will be called when it completes */
-                    mBluetoothLeService.readLedCharacteristic();
-                    Log.d(TAG, "Services Discovered");
+                     Log.d(TAG, "Services Discovered");
                     break;
-                case BluetoothLeService.ACTION_DATA_AVAILABLE:
-                    String[] resultArray = intent.getStringArrayExtra(BluetoothLeService.EXTRA_DATA);
-                    switch (resultArray[0]) {
-                        case BluetoothLeService.ledCharacteristicUUID:
-                            /* Check the state of the LED on the device and update the slider switch */
-                            if (resultArray[1].equals("1")) {
-                                led_switch.setChecked(true);
-                            } else {
-                                led_switch.setChecked(false);
-                            }
-                            break;
-                        case BluetoothLeService.capsenseCharacteristicUUID:
-                            /* Update CapSense value display on the screen */
-                            if(resultArray[1].equals("65535")) { // No Touch results in 0xFFFF
-                                mCapsenseValue.setText("No Touch");
-                            } else {
-                                mCapsenseValue.setText(resultArray[1]);
-                            }
-                            break;
+                case PSoCCapSenseLedService.ACTION_DATA_RECEIVED:
+                    // This is called after a notify or a read completes
+                    // Check LED switch Setting
+                    if(mPSoCCapSenseLedService.getLedSwitchState()){
+                        led_switch.setChecked(true);
+                    } else {
+                        led_switch.setChecked(false);
+                    }
+                    // Get CapSense Slider Value
+                    String CapSensePos = mPSoCCapSenseLedService.getCapSenseValue();
+                    if (CapSensePos.equals("-1")) {  // No Touch returns 0xFFFF which is -1
+                        mCapsenseValue.setText(R.string.Question);
+                    } else {
+                        mCapsenseValue.setText(CapSensePos);
                     }
                 default:
                     break;
